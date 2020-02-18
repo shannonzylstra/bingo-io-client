a67e8ebabdb77b65b86cb0a3c6c596636481fc98
shannonzylstra <shannonzylstra@gmail.com>
2020-02-18 13:33:45 -0800

    Got it working to display weather

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
+import './App.css';
+
+function App () {
+    let [ response, setResponse ] = useState(false);
+    let [ endpoint, setEndpoint ] = useState("http://127.0.0.1:4001");
+
+    // Socket Stuff
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => setResponse(data));
+
+    return (
+        <div style={{ textAlign: "center" }}>
+          {response
+              ? <p>
+                The temperature in Florence is: {response} °F
+              </p>
+              : <p>Loading...</p>}
+        </div>
+    );
+}
+export default App;
+
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:4001"
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => this.setState({ response: data }));
+  }
+
+  render() {
+    const { response } = this.state;
+    return (
+        <div style={{ textAlign: "center" }}>
+          {response
+              ? <p>
+                The temperature in Florence is: {response} °F
+              </p>
+              : <p>Loading...</p>}
+        </div>
+    );
+  }
+}
 export default App;
